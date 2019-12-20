b7118ed2677987aefc0d72e959752f4c78062322
Pramod <39467947+pomtom@users.noreply.github.com>
2019-12-20 15:50:08 +0000

    Test 1
    Test 1

--- AzFunction/Function1.cs
+++ AzFunction/Function1.cs
@@ -10,6 +10,7 @@ namespace AzFunction
         public static void Run([BlobTrigger("inputcsv/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, TraceWriter log)
         {
             log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            log.Info($"Pramod Lawate");
         }
     }
 }
